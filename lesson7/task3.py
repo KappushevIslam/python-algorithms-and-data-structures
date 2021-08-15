34617419509c8af340582cba1c5e443e68ddcfe1
KappushevIslam <77545933+KappushevIslam@users.noreply.github.com>
2021-08-16 01:31:51 +0300

    Update task3.py

--- lesson7/task3.py
+++ lesson7/task3.py
@@ -27,38 +27,41 @@ print(f'Исходный список - {lst_obj}\n'
 
 # ------- Метод с сортировкой -------
 # Нашёл этот метод быстрой сортировки в интернете
-def quicksort(alist, start, end):
-    if end - start > 1:
-        p = partition(alist, start, end)
-        quicksort(alist, start, p)
-        quicksort(alist, p + 1, end)
+def partition(array, begin, end):
+    pivot = begin
+    for i in range(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
 
 
-def partition(alist, start, end):
-    pivot = alist[start]
-    i = start + 1
-    j = end - 1
+def quick_sort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
 
-    while True:
-        while i <= j and alist[i] <= pivot:
-            i = i + 1
-        while i <= j and alist[j] >= pivot:
-            j = j - 1
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot - 1)
+        _quicksort(array, pivot + 1, end)
 
-        if i <= j:
-            alist[i], alist[j] = alist[j], alist[i]
-        else:
-            alist[start], alist[j] = alist[j], alist[start]
-            return j
+    return _quicksort(array, begin, end)
 
 
 n2 = int(input('Введите n: '))
 alist = [random.randint(1, 1000) for j in range(2*n2+1)]
-quicksort(alist, 0, len(alist))
+quick_sort(alist)
 print(f'Отсортированный список: {alist}\n', end='')
 print(f'Медиана списка - {alist[n2]}\n'
       f'Медиана через модуль statistics - {median(alist)}')
-print(timeit('quicksort(alist[:], 0, len(alist))', globals=globals(), number=1000000))
+
+print(timeit('quick_sort(alist[:])', globals=globals(), number=1000000))
+print(timeit('find_median1(lst_obj[:])', globals=globals(), number=1000000))
+print(timeit('median(alist[:])', globals=globals(), number=1000000))
+
 
 """
 Введите n: 4
@@ -70,5 +73,11 @@ print(timeit('quicksort(alist[:], 0, len(alist))', globals=globals(), number=100
 Отсортированный список: [70, 349, 471, 602, 650, 753, 876]
 Медиана списка - 602
 Медиана через модуль statistics - 602
-16.10177044
+
+9.29877296 - quick_sort
+2.773033999999999 - find_median
+1.53841624 - median
+
+Median быстрее остальных. Своя свобственная функция тоже весьма неплоха.
+А решение через быструю сортировку наиболее большое и медленное.
 """
